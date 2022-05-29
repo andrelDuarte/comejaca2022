371e0da70e2ced15fe4cf1206e5d8051f659d343
Andre Luiz <and969696@outlook.com>
2022-05-29 11:08:52 -0300

    feat: add nodemailer

---
+++ src/shared/container/providers/MailProvider/implementations/HostGatorMailProvider.ts
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+
+import { logger } from '@shared/utils/logger';
+
+import { ISendMailDTO } from '../dtos/ISendMailDTO';
+import { IMailProvider } from '../models/IMailProvider';
+
+class HostGatorMailProvider implements IMailProvider {
+  public async sendMail({ from, to, subject, html, cc }: ISendMailDTO) {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      name: 'brad@comejaca.org.br',
+      port: 587,
+      /* secure: true, */
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    try {
+      const info = await transporter.sendMail({
+        from,
+        to,
+        subject,
+        html,
+        cc
+      });
+      logger.debug(info);
+    } catch (err) {
+      logger.error('Sendgrid', err);
+    }
+  }
+}
+
+export { HostGatorMailProvider };
